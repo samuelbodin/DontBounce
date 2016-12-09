0c98aca3a3af720958fb13cdef80517e79678be2
deerel <deerel@gmail.com>
2016-12-09 15:49:23 +0100

    Minor fix on WorldBackground

--- core/src/com/mygdx/game/Basics/WorldBackground.java
+++ core/src/com/mygdx/game/Basics/WorldBackground.java
@@ -15,18 +15,23 @@ public class WorldBackground extends Drawable
     Sprite m_sprite;
     private float m_viewportWidth;
     private float m_viewportHeight;
-    private float m_lagFactor = 0.995f;
+    private float m_lagFactor = 0.997f;
     public WorldBackground(float viewportWidth, float viewportHeight )
     {
-        m_viewportWidth = viewportWidth*1.2f;
+        m_viewportWidth = viewportWidth;
         m_viewportHeight = viewportHeight;
         m_sprite = new Sprite(new Texture("appbg.png"));
-        m_sprite.setSize(m_viewportWidth,m_viewportWidth*(m_sprite.getHeight()/m_sprite.getWidth()));
-        m_sprite.setPosition(-m_sprite.getWidth()/2, -(m_sprite.getHeight()/2));
+
+        // Make background a bit larger than viewport to allow sideways parallax
+        m_sprite.setSize(m_viewportWidth*1.2f,m_viewportWidth*1.2f*(m_sprite.getHeight()/m_sprite.getWidth()));
+
+        // Center on width and align background top with viewport top.
+        m_sprite.setPosition((m_viewportWidth-m_sprite.getWidth())/2,(m_viewportHeight-m_sprite.getHeight()));
     }
 
     public void setPosition(Vector2 pos)
     {
+        // Attach background to camera and scroll background up with a factor
         m_sprite.setPosition(m_sprite.getX(), m_sprite.getY() + (pos.y*m_lagFactor));
     }
 
