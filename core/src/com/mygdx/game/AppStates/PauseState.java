53da766e433e4e42e9c1f825d1bf8ba21644b6c1
Pontus <pontus.wed@gmail.com>
2016-12-17 14:13:42 +0100

    Pause design and button instead of screen touch to return. Removed obsolete texture.pack

--- core/src/com/mygdx/game/AppStates/PauseState.java
+++ core/src/com/mygdx/game/AppStates/PauseState.java
@@ -1,11 +1,22 @@
 package com.mygdx.game.AppStates;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.glutils.GLFrameBuffer;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.game.App;
 import com.mygdx.game.Ball.Ball;
@@ -20,37 +31,70 @@ public class PauseState extends State
     private StateManager m_sm;
     private static final float m_viewportWidth = App.m_worldW;
     private static final float m_viewportHeight = App.m_worldH;
-    private Viewport m_viewport;
-    private Sprite m_box;
-    private Ball m_ball;
-    private BitmapFont font;
-    private boolean delta = false;
+    private Stage m_stage;
+    private TextureAtlas m_icons;
+    private Skin m_skin;
+    private BitmapFont m_font;
+    private TextButton m_return;
+    private TextButton.TextButtonStyle m_returnSkin;
+    private Table m_layoutTable;
+    private Label m_header;
+    private Label.LabelStyle m_headerSkin;
 
     public PauseState(StateManager sm)
     {
         super(sm);
         m_sm = sm;
-        font = new BitmapFont(Gdx.files.internal("slackeyfont/slackey.fnt"),Gdx.files.internal("slackeyfont/slackey.png"),false);
-        m_box = new Sprite(new Texture("obs5.png"));
+        m_font = new BitmapFont(Gdx.files.internal("slackeyfont/slackey100.fnt"));
 
-        m_viewport = new FitViewport(m_viewportWidth, m_viewportHeight, m_cam);
-        m_viewport.setScreenBounds(0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
-        m_viewport.apply(true);
+        m_stage = new Stage(new StretchViewport(m_viewportWidth, m_viewportHeight));
 
-        m_box.setOrigin(0,0);
-        m_box.setScale(m_viewportWidth/m_box.getWidth());
-        m_box.setPosition(0,m_viewportHeight/2);
+        //Move this to AssetLoader in the future
+        m_icons = new TextureAtlas("icons/icons.pack");
+        m_skin = new Skin(m_icons);
 
-        m_ball = new Ball(m_box.getX(), m_box.getY(), m_viewportWidth/40);
-        m_cam.setBall(m_ball);
-        m_cam.update();
+        m_layoutTable = new Table();
+        m_layoutTable.setFillParent(true);
+
+        m_returnSkin = new TextButton.TextButtonStyle();
+        m_headerSkin = new Label.LabelStyle(m_font, Color.WHITE);
+
+        m_returnSkin.up = m_skin.getDrawable("btn");
+        m_returnSkin.down = m_skin.getDrawable("btn");
+        m_returnSkin.font = m_font;
+
+        m_header = new Label("PAUSED", m_headerSkin);
+        m_return = new TextButton("Return to game", m_returnSkin);
+
+        m_header.setFontScale(0.7f);
+        m_return.getLabel().setFontScale(0.5f);
+
+        m_layoutTable.add(m_header);
+        m_layoutTable.row();
+        m_layoutTable.add(m_return).width(600f).height(100f);
+
+        m_stage.addActor(m_layoutTable);
+
+        Gdx.input.setInputProcessor(m_stage);
+        setupClickListeners();
+    }
+
+    private void setupClickListeners()
+    {
+        m_return.addListener(new ChangeListener()
+        {
+            @Override
+            public void changed(ChangeEvent event, Actor actor)
+            {
+                Gdx.app.log("PW", "return click");
+                m_sm.pop();
+            }
+        });
     }
 
     @Override
     public void update(float dt)
     {
-
-            handleInput();
 
     }
 
@@ -58,25 +102,21 @@ public class PauseState extends State
     public void render(SpriteBatch sr)
     {
         sr.setProjectionMatrix(m_cam.combined);
-
-        m_box.draw(sr);
-        font.draw(sr, "PAUS", App.m_worldW/2,App.m_worldH/2);
+        m_stage.draw();
     }
 
     @Override
     public void handleInput()
     {
-        if(Gdx.input.justTouched()) {
-            m_sm.pop();
-            //m_sm.set(new UserTestMenu(m_sm));
-        }
+
     }
 
     @Override
     public void dispose()
     {
-        m_box.getTexture().dispose();
-        font.dispose();
+        m_icons.dispose();
+        m_skin.dispose();
+        m_font.dispose();
         Gdx.app.log("RL", "PausState disposar");
     }
 
