e2a8738fa7198d8fea2b6f7eabd251dd1497006a
JoelSöderman <joel.soederman@gmail.com>
2016-12-18 22:25:14 +0100

    Nothing new really, just added a parameter to a RIcstate  constructor call.

--- core/src/com/mygdx/game/AppStates/LevelFinishedState.java
+++ core/src/com/mygdx/game/AppStates/LevelFinishedState.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.game.App;
+import com.mygdx.game.Basics.LevelData;
 
 /**
  * Created by Rickard on 2016-12-09.
@@ -36,7 +37,7 @@ public class LevelFinishedState extends State
     {
         if(Gdx.input.justTouched())
         {
-            m_sm.set(new RicState(m_sm));
+            m_sm.set(new RicState(m_sm, new LevelData()));
         }
     }
 
