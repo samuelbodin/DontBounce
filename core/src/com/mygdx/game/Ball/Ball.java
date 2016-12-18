321a000001d1faa90ca22e7b4c310f5743ee216d
JoelSöderman <joel.soederman@gmail.com>
2016-12-18 22:22:13 +0100

    Cut the cord from BallstateMovable to enable other BallstateDesigns
    We had a disheartening big mess going where Ball, Ballstate and
    BallstateMovable/BallstateFast where coupled up to the extent that they
    strangled each other and prevented me doing what I was going to do in
    this first place.

--- core/src/com/mygdx/game/Ball/Ball.java
+++ core/src/com/mygdx/game/Ball/Ball.java
@@ -8,43 +8,53 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 import com.mygdx.game.App;
 import com.mygdx.game.Basics.Drawable;
+import com.mygdx.game.Basics.LevelData;
 
 public class Ball
 {
     boolean m_isOnGround = false;
-    float m_radius = 0.0f;
+    float m_radius = 0f;
     Vector2 m_position = null;
     Vector2 m_velocity = null;
     private BallState m_state = null;
     private BallPositionHistory m_history = null;
+    float m_worldW = 0f;
     int m_iterator = 0;
 
-    public Ball (float x, float y, float r)
+    public Ball (float x, float y, float r, float worldW)
     {
+        m_worldW = worldW;
         m_position = new Vector2(x,y);
         m_velocity = new Vector2(0,0);
         m_radius = r;
-        m_state = new BallStateMoveable(this);
         m_history = new BallPositionHistory(3);
         m_history.addToHistory(new com.mygdx.game.Basics.Circle(m_position,m_radius, m_iterator++));
     }
 
-    public void doTrail()
+    public Ball (float x, float y, float r, float worldW, LevelData ld)
     {
-        m_state.doTrail = true;
+        this(x, y, r, worldW);
+        m_state = new BallStateMoveable(this, ld.m_ballGravity, ld.m_ballMaxSpeed, ld.m_ballSensitivity);
     }
 
-    public void setState(BallState s)
+    void setState(BallState s)
     {
         m_state = s;
     }
+
     public void update(float dt)
     {
-        //m_state.update(dt);
+        m_state.update(dt);
+
+        if(m_velocity.y < m_state.m_maxSpeed)
+        {
+            m_velocity.y = m_state.m_maxSpeed;
+        }
 
         if(m_isOnGround)
         {
             m_isOnGround = false;
+            m_velocity.scl(1,0);
         }
         else
         {
@@ -52,17 +62,36 @@ public class Ball
             m_velocity.add(0,m_state.m_gravity);
         }
 
-        if(m_velocity.y < m_state.m_maxSpeed)
-        {
-            m_velocity.y = m_state.m_maxSpeed;
-        }
-
         m_position.add(m_velocity.x*dt,m_velocity.y*dt);
 
-        m_state.update(dt);
+        handleEdges();
+
+        m_state.updateSprite();
 
         m_history.addToHistory(new com.mygdx.game.Basics.Circle(m_position, m_radius, m_iterator++));
     }
+    private void handleEdges()
+    {
+        if(m_position.x-m_radius < 0)
+        {
+            m_velocity.scl(0,1);
+            m_position.x = m_radius;
+        }
+        else if(m_position.x+m_radius > m_worldW)
+        {
+            m_velocity.scl(0,1);
+            m_position.x = m_worldW - m_radius;
+        }
+    }
+
+    public void setDeltaMove(float deltaMove)
+    {
+        if(m_state.hasDeltaMove())
+        {
+            m_state.setDeltaMove(deltaMove);
+        }
+    }
+
     public void render(SpriteBatch sb)
     {
         m_state.render(sb);
@@ -70,10 +99,6 @@ public class Ball
     public void onCollision(Vector2 pos, int side)
     {
         m_state.onCollision(pos, side);
-    }
-    public void onCollision(Vector2 pos, int side, Vector2 pos1, Vector2 pos2)
-    {
-        m_state.onCollision(pos, side, pos1, pos2);
     }
     public Vector2 getPosition()
     {
