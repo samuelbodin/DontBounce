31c367b7aae670a36c86169c855e165c414f066b
deerel <deerel@gmail.com>
2016-12-19 08:59:29 +0100

    Added inverted level

--- core/src/com/mygdx/game/Basics/LevelGenerator.java
+++ core/src/com/mygdx/game/Basics/LevelGenerator.java
@@ -31,24 +31,24 @@ public class LevelGenerator
         m_worldWidth = App.m_worldW;
         m_worldHeight = worldHeight;
 
-        m_minObstacleYSpace = m_worldWidth/(obstacleMinSpacingFactor/10);
-        m_obstacleYSpaceFactor = (m_worldWidth*2)/(obstacleSeparationFactor/10);
-        m_obstacleWidth = (m_worldWidth/3)*(obstacleSizeFactor/10);
+        m_minObstacleYSpace = m_worldWidth/(obstacleMinSpacingFactor/15);
+        m_obstacleYSpaceFactor = (m_worldWidth/2)/(obstacleSeparationFactor/10);
+        m_obstacleWidth = (m_worldWidth/4)*(obstacleSizeFactor/15);
         m_snapMargin = obstacleSnapMargin;
 
         m_collidables = new Array<Collidable>();
         m_noise = new SimplexNoise(seed);
-        generate();
     }
 
     private void generate()
     {
-        // Course borders
         int i=0;
         do
         {
-            m_obstacleX = Math.abs((float) m_noise.noise(i + 50, i + 25)) % 1f;
-            m_obstacleY = Math.abs((float) m_noise.noise(i + 25, i + 50)) % 1f;
+            //m_obstacleX = Math.abs((float) m_noise.noise(i + 50, i + 25)) % 1f;
+            //m_obstacleY = Math.abs((float) m_noise.noise(i + 25, i + 50)) % 1f;
+            m_obstacleX = Math.abs((float) m_noise.noise(1, i*0.1)) % 1f;
+            m_obstacleY = m_obstacleX;
             if (m_obstacleY < 0.2f)
             {
                 m_obstacleY = 0.2f;
@@ -70,6 +70,45 @@ public class LevelGenerator
             }
             m_obstacleY = m_obstacleY * m_obstacleYSpaceFactor + m_minObstacleYSpace;
             m_collidables.add(new StaticObstacle(m_obstacleX, m_lastObstableY - m_obstacleY, m_obstacleWidth, 32));
+            m_lastObstableY -= m_obstacleY;
+            m_lastObstacleX = m_obstacleX;
+            i++;
+        } while(m_lastObstableY > -m_worldHeight+(m_minObstacleYSpace*2));
+    }
+
+    private void generateInverted()
+    {
+        int i=0;
+        do
+        {
+            //m_obstacleX = Math.abs((float) m_noise.noise(i + 50, i + 25)) % 1f;
+            //m_obstacleY = Math.abs((float) m_noise.noise(i + 25, i + 50)) % 1f;
+            m_obstacleX = Math.abs((float) m_noise.noise(1, i*0.1)) % 1f;
+            m_obstacleY = m_obstacleX;
+            if (m_obstacleY < 0.2f)
+            {
+                m_obstacleY = 0.2f;
+            }
+
+            m_obstacleX = (m_obstacleX * m_worldWidth);
+
+            if(Math.abs(m_obstacleX - m_lastObstacleX) <  m_worldWidth/6)
+            {
+                m_obstacleX = ((m_obstacleX+m_worldWidth/6))%m_worldWidth;
+            }
+
+            if (m_obstacleX + m_obstacleWidth > m_worldWidth - m_snapMargin)
+            {
+                m_obstacleX = m_worldWidth - m_obstacleWidth;
+            } else if (m_obstacleX < m_snapMargin)
+            {
+                m_obstacleX = 0;
+            }
+            m_obstacleY = m_obstacleY * m_obstacleYSpaceFactor + m_minObstacleYSpace;
+            //m_collidables.add(new StaticObstacle(m_obstacleX, m_lastObstableY - m_obstacleY, m_obstacleWidth, 32));
+            m_collidables.add(new StaticObstacle(0, m_lastObstableY - m_obstacleY, m_obstacleX, 32));
+            m_collidables.add(new StaticObstacle(m_obstacleX+m_obstacleWidth, m_lastObstableY - m_obstacleY, m_worldWidth-(m_obstacleX+m_obstacleWidth), 32));
+
             m_lastObstableY -= m_obstacleY;
             m_lastObstacleX = m_obstacleX;
             i++;
