ba7750733b78fdce256bbee1ff08262e9ed89106
deerel <deerel@gmail.com>
2016-12-19 12:33:58 +0100

    Added TimeHandler

---
+++ core/src/com/mygdx/game/Basics/TimeHandler.java
@@ -0,0 +1,80 @@
+package com.mygdx.game.Basics;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+/**
+ * Created by Rickard on 2016-12-19.
+ */
+
+public class TimeHandler
+{
+
+    private float m_timer;
+    private boolean m_running;
+    private Vector2 m_drawPosition;
+    private BitmapFont m_font;
+
+    public TimeHandler(float x, float y)
+    {
+
+        m_font = new BitmapFont(Gdx.files.internal("slackeyfont/slackey100.fnt"));
+        m_font.setColor(1,1,1,0.5f);
+        m_font.getRegion().getTexture().setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+        m_font.getData().setScale(0.5f);
+
+        m_drawPosition = new Vector2(x,y);
+        m_timer = 0f;
+        m_running = false;
+    }
+
+    public void start()
+    {
+        m_running = true;
+    }
+
+    public void stop()
+    {
+        m_running = false;
+    }
+
+    public boolean isRunning()
+    {
+        return m_running;
+    }
+
+    public float getTime()
+    {
+        return m_timer;
+    }
+
+    public String getTimeString()
+    {
+        return String.format("%.2f", m_timer);
+    }
+
+    public void render(SpriteBatch sb)
+    {
+        m_font.draw(sb, String.format("%.2f", m_timer), m_drawPosition.x, m_drawPosition.y);
+    }
+
+    public void update(float dt, Vector2 deltaMove)
+    {
+        if(m_running)
+        {
+            m_timer += dt;
+        }
+
+        m_drawPosition.y += deltaMove.y;
+    }
+
+    public void dispose()
+    {
+        m_font.dispose();
+    }
+
+}
