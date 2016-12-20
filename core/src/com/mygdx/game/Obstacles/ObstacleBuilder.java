c47c7950344e6a57d5396771328ca906ef171626
deerel <deerel@gmail.com>
2016-12-20 22:23:48 +0100

    Particles on bounce

--- core/src/com/mygdx/game/Obstacles/ObstacleBuilder.java
+++ core/src/com/mygdx/game/Obstacles/ObstacleBuilder.java
@@ -1,11 +1,15 @@
 package com.mygdx.game.Obstacles;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
 import com.mygdx.game.Basics.Drawable;
 
 /**
@@ -18,12 +22,17 @@ public class ObstacleBuilder extends Drawable
     Sprite m_start, m_body, m_end;
     float m_posX, m_posY;
 
-    Sprite m_crack;
-    Texture[] m_texture;
+    //Sprite m_crack;
+    //Texture[] m_texture;
     boolean m_isCracked = false;
-    float m_frameTime = 0;
-    int m_currentFrame = 0;
-    int m_numOfFrames = 5;
+    //float m_frameTime = 0;
+    //int m_currentFrame = 0;
+    //int m_numOfFrames = 32;
+    Vector2 m_collisionPos = null;
+    Array<TextureRegion> runningAnimation;
+    TextureAtlas atlas;
+    Animation animation;
+    float elapsedTime = 0;
 
     public ObstacleBuilder(float x, float y, float width, float height, Color tint)
     {
@@ -40,51 +49,54 @@ public class ObstacleBuilder extends Drawable
 
         m_start.setSize(height, height);
         m_end.setSize(height, height);
-        m_body.setSize(width-(2*height),height);
+        m_body.setSize(width - (2 * height), height);
 
-        m_start.setPosition(x,y);
-        m_body.setPosition(x+m_start.getWidth(),y);
-        m_end.setPosition(m_body.getX()+m_body.getWidth(),y);
+        m_start.setPosition(x, y);
+        m_body.setPosition(x + m_start.getWidth(), y);
+        m_end.setPosition(m_body.getX() + m_body.getWidth(), y);
 
-        Color c = new Color(0.25f,0.5f,0.75f,1);
+        Color c = new Color(0.25f, 0.5f, 0.75f, 1);
         m_start.setColor(tint);
         m_body.setColor(tint);
         m_end.setColor(tint);
-
+/*
         m_texture = new Texture[m_numOfFrames];
-        for(int i = 0; i < m_texture.length; i++)
+        for (int i = 0; i < m_texture.length; i++)
         {
-            m_texture[i] = new Texture("animation/bounce0"+(i+1)+".png");
+            m_texture[i] = new Texture("animation/bounce01.png");
         }
         m_crack = new Sprite(m_texture[0]);
-        m_crack.setSize(32,32);
+        m_crack.setSize(32, 32);
         m_crack.setColor(tint);
+*/
+        atlas = new TextureAtlas(Gdx.files.internal("gameObjects/splash.pack"));
 
+        Array<TextureAtlas.AtlasRegion> splashRegion = atlas.findRegions("splash");
+
+        animation = new Animation(0.05f, splashRegion);
     }
 
     public void setCracked(Vector2 pos)
     {
-        m_crack.setPosition(pos.x,pos.y);
+        //m_crack.setPosition(pos.x, pos.y);
+        m_collisionPos = new Vector2(pos);
         m_isCracked = true;
     }
 
     @Override
     public void update(float dt)
     {
-        if(m_isCracked)
+        if (m_isCracked)
         {
-            if(m_frameTime > 0.05 && m_currentFrame < m_numOfFrames-1)
-            {
-                m_currentFrame = (m_currentFrame+1);
-                m_crack.setTexture(m_texture[m_currentFrame]);
-                m_frameTime = 0;
-            }
-            m_frameTime += dt;
 
-            if(m_currentFrame >= m_numOfFrames-1)
+            if(animation.isAnimationFinished(elapsedTime))
             {
-                m_currentFrame = 0;
+                elapsedTime = 0;
                 m_isCracked = false;
+            }
+            else
+            {
+                elapsedTime += dt;
             }
         }
     }
@@ -95,19 +107,17 @@ public class ObstacleBuilder extends Drawable
         m_start.draw(sb);
         m_body.draw(sb);
         m_end.draw(sb);
-        if(m_isCracked)
+        if (m_isCracked)
         {
-            m_crack.draw(sb);
+            //m_crack.draw(sb);
+            sb.draw(animation.getKeyFrame(elapsedTime), m_collisionPos.x, m_collisionPos.y);
         }
     }
 
     @Override
     public void dispose()
     {
-        m_crack.getTexture().dispose();
-        for(Texture t : m_texture)
-        {
-            t.dispose();
-        }
+        atlas.dispose();
     }
+
 }
