fac395517f7c6ee08beaf092869519a69d06c98d
deerel <deerel@gmail.com>
2016-12-21 10:29:03 +0100

    Deleted images

--- core/src/com/mygdx/game/Obstacles/ObstacleBuilder.java
+++ core/src/com/mygdx/game/Obstacles/ObstacleBuilder.java
@@ -20,12 +20,7 @@ public class ObstacleBuilder extends Drawable
 
     Sprite m_start, m_body, m_end;
     float m_posX, m_posY;
-/*
-    boolean m_isCracked = false;
-    Vector2 m_collisionPos = null;
-    TextureAtlas m_atlas;
-    Animation m_animation;
-    float m_elapsedTime = 0;*/
+
 
     public ObstacleBuilder(float x, float y, float width, float height, Color tint)
     {
@@ -35,10 +30,6 @@ public class ObstacleBuilder extends Drawable
         m_start = new Sprite(new Texture("flatbwleft.png"));
         m_body = new Sprite(new Texture("flatbwbody.png"));
         m_end = new Sprite(new Texture("flatbwright.png"));
-
-        //m_start.getTexture().setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
-        //m_body.getTexture().setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
-        //m_end.getTexture().setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
 
         m_start.setSize(height, height);
         m_end.setSize(height, height);
@@ -53,33 +44,13 @@ public class ObstacleBuilder extends Drawable
         m_body.setColor(tint);
         m_end.setColor(tint);
 
-        /*m_atlas = new TextureAtlas(Gdx.files.internal("gameObjects/splash.pack"));
-        Array<TextureAtlas.AtlasRegion> splashRegion = m_atlas.findRegions("splash");
-        m_animation = new Animation(0.05f, splashRegion);*/
     }
 
-/*    public void setCracked(Vector2 pos)
-    {
-        m_collisionPos = new Vector2(pos);
-        m_isCracked = true;
-    }*/
 
     @Override
     public void update(float dt)
     {
-/*        if (m_isCracked)
-        {
 
-            if(m_animation.isAnimationFinished(m_elapsedTime))
-            {
-                m_elapsedTime = 0;
-                m_isCracked = false;
-            }
-            else
-            {
-                m_elapsedTime += dt;
-            }
-        }*/
     }
 
     @Override
@@ -88,16 +59,12 @@ public class ObstacleBuilder extends Drawable
         m_start.draw(sb);
         m_body.draw(sb);
         m_end.draw(sb);
-/*        if (m_isCracked)
-        {
-            sb.draw(m_animation.getKeyFrame(m_elapsedTime), m_collisionPos.x, m_collisionPos.y);
-        }*/
     }
 
     @Override
     public void dispose()
     {
-        /*m_atlas.dispose();*/
+
     }
 
 }
