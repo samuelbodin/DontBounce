5a7b821b828ed203e1e61391b0f60dcd4f5340b6
deerel <deerel@gmail.com>
2016-12-22 18:51:45 +0100

    Added chapter Two

--- core/src/com/mygdx/game/levels/LevelDictionary.java
+++ core/src/com/mygdx/game/levels/LevelDictionary.java
@@ -12,7 +12,7 @@ public class LevelDictionary
     private LevelDictionary()
     {
         m_data = new ArrayList<LevelData>();
-       loadChapterOne();
+        loadChapterOne();
 
     }
 
@@ -77,6 +77,46 @@ public class LevelDictionary
 
         m_data.add(
             new LevelData(9, 1, 9, true, 25, 13, 11, 80, true, -12f, -1500f, 2f)
+        );
+    }
+
+
+    private void loadChapterTwo()
+    {
+        m_data.add(
+                new LevelData(1, 1, 1, true, 13, 12, 10, 80, false, -10f, -1500f, 2f)
+        );
+
+        m_data.add(
+                new LevelData(2, 1, 2, true, 22, 13, 11, 80, false, -10f, -1500f, 2f)
+        );
+
+        m_data.add(
+                new LevelData(3, 1, 3, true, 35, 13, 11, 80, true, -10f, -1500f, 2f)
+        );
+
+        m_data.add(
+                new LevelData(4, 1, 4, true, 14, 14, 12, 80, false, -11f, -1500f, 2f)
+        );
+
+        m_data.add(
+                new LevelData(5, 1, 5, true, 15, 15, 13, 80, false, -11f, -1500f, 2f)
+        );
+
+        m_data.add(
+                new LevelData(6, 1, 6, true, 30, 13, 11, 80, true, -11f, -1500f, 2f)
+        );
+
+        m_data.add(
+                new LevelData(7, 1, 7, true, 16, 16, 14, 80, false, -12f, -1500f, 2f)
+        );
+
+        m_data.add(
+                new LevelData(8, 1, 8, true, 17, 17, 18, 80, false, -12f, -1500f, 2f)
+        );
+
+        m_data.add(
+                new LevelData(9, 1, 9, true, 25, 13, 11, 80, true, -12f, -1500f, 2f)
         );
     }
 }
