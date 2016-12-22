dd30c441b62cf3880dc5ac857aa83b0374d9eac8
deerel <deerel@gmail.com>
2016-12-22 18:50:13 +0100

    Chapter Two

---
+++ core/src/com/mygdx/game/levels/ChapterTwo.java
@@ -0,0 +1,23 @@
+package com.mygdx.game.levels;
+
+import com.badlogic.gdx.graphics.Color;
+import com.mygdx.game.Basics.AssetLoader;
+
+/**
+ * Created by Rickard on 2016-12-22.
+ */
+
+public class ChapterTwo extends Chapter
+{
+    public ChapterTwo()
+    {
+        super(1);
+
+        m_backgrounds.add(AssetLoader.spacebg01);
+        m_backgrounds.add(AssetLoader.spacebg02);
+        m_backgrounds.add(AssetLoader.spacebg03);
+        m_foreground  = AssetLoader.spacebgforeground;
+
+        m_tint = new Color(0.75f,0.5f,0.25f,1);
+    }
+}
