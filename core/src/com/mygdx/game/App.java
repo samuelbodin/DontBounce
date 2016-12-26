b1bb0a063c982627d3ee7c0d9a65b376e90d051b
deerel <deerel@gmail.com>
2016-12-26 12:09:10 +0100

    Minor fix

--- core/src/com/mygdx/game/App.java
+++ core/src/com/mygdx/game/App.java
@@ -2,8 +2,10 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.FPSLogger;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.profiling.GLProfiler;
 import com.mygdx.game.AppStates.StateManager;
 import com.mygdx.game.AppStates.MenuState;
 import com.mygdx.game.Basics.AssetLoader;
@@ -15,6 +17,8 @@ public class App extends ApplicationAdapter
     private SpriteBatch m_sb = null;
 	private Config m_config = null;
 
+	private FPSLogger fpslogger;
+
 	@Override
 	public void create ()
 	{
@@ -24,18 +28,46 @@ public class App extends ApplicationAdapter
         m_sb = new SpriteBatch();
         m_sm.set(new MenuState(m_sm));
 		Gdx.gl.glClearColor(0, 0, 0, 1);
+		// Profiling stuff
+		fpslogger = new FPSLogger();
+		//GLProfiler.enable();
 	}
 
 	@Override
 	public void render ()
 	{
 		super.render();
-        m_sm.update(Gdx.graphics.getDeltaTime());
+		m_sm.update(Gdx.graphics.getDeltaTime());
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         m_sb.begin();
 		m_sb.enableBlending();
         m_sm.render(m_sb);
         m_sb.end();
+
+		// Profiling stuff
+
+		//fpslogger.log();
+		//Gdx.app.log("PROFILING", "DrawCalls: " + GLProfiler.drawCalls + ", Calls:" + GLProfiler.calls + ". TextureBindings: " + GLProfiler.textureBindings);
+		//GLProfiler.reset();
+		//Gdx.app.log("PROFILING", "Delta Time: " + Gdx.graphics.getDeltaTime());
+
+		//System.out.print("\nFPS: " + Gdx.graphics.getFramesPerSecond() + "\n");
+
+		if(Gdx.graphics.getDeltaTime() < 1f/30f)
+		{
+			//System.out.print("DeltaTime: " + Gdx.graphics.getDeltaTime() + " s\n");
+			float sleep = (1f/30f-Gdx.graphics.getDeltaTime())*1000;
+			//System.out.print("sleep: " + sleep + " ms\n");
+			try
+			{
+				Thread.sleep((long) sleep);
+			}
+			catch (InterruptedException e)
+			{
+				System.out.print("Error...");
+				e.printStackTrace();
+			}
+		}
 	}
 
 	@Override
@@ -50,5 +82,8 @@ public class App extends ApplicationAdapter
 		super.dispose();
 		AssetLoader.dispose();
 		Gdx.app.log("RL", "App disposar");
+
+		// Profiling stuff
+		//GLProfiler.disable();
 	}
 }
