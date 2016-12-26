bfee7e5d3246f6ddb08c646abe00b1c707b0217b
JoelSöderman <joel.soederman@gmail.com>
2016-12-26 16:29:11 +0100

    Cleaned up some and got rid of all warnings

--- core/src/com/mygdx/game/Basics/Config.java
+++ core/src/com/mygdx/game/Basics/Config.java
@@ -1,13 +1,8 @@
 package com.mygdx.game.Basics;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Preferences;
-import com.mygdx.game.levels.Chapter;
-import com.mygdx.game.levels.ChapterOne;
-import com.mygdx.game.levels.ChapterTwo;
 import com.mygdx.game.levels.LevelManager;
 
-import java.util.ArrayList;
 
 public class Config
 {
@@ -18,16 +13,12 @@ public class Config
     public final int m_worldH = 1280;
     public int m_screenW = 0;
     public int m_screenH = 0;
-    public float m_aspectR = 0.0f;
     private int m_returnCount = 0;
-
-    public Preferences m_preferences = null;
 
     public Config()
     {
         m_screenH = Gdx.graphics.getHeight();
         m_screenW = Gdx.graphics.getWidth();
-        m_aspectR = (m_screenH/m_screenW);
 
         m_lm = new LevelManager();
         m_ah = new AudioHandler(checkMutePreferences());
