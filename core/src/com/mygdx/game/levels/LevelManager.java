c87e5ac116f2fb2e342bf451a45a10a2bd649f8e
Samuel Bodin <samuel.bodin@live.se>
2017-01-05 14:38:34 +0100

    Unlocked all levels

--- core/src/com/mygdx/game/levels/LevelManager.java
+++ core/src/com/mygdx/game/levels/LevelManager.java
@@ -12,8 +12,8 @@ public class LevelManager
 {
     private Preferences m_preferences = null;
     private ArrayList<Chapter> m_chapters = null;
-    private int m_unlockedLevel = 1;
-    private int m_currentChapter = 1;
+    private int m_unlockedLevel = 18;
+    private int m_currentChapter = 2;
 
 
     public LevelManager()
@@ -25,7 +25,7 @@ public class LevelManager
         m_chapters.add(new ChapterOne());
         m_chapters.add(new ChapterTwo());
 
-        m_unlockedLevel = m_preferences.getInteger("unlocked_level", 1);
+        m_unlockedLevel = m_preferences.getInteger("unlocked_level", 18);
         m_currentChapter = getIdOfLastUnlockedChapter();
 
     }
